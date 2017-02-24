78a8e447a2f733c146e8d3c2929c4c591497092c
temason <temasonos@gmail.com>
2017-02-24 18:37:08 +0300

    changed names

--- python/term4/pr2/ex2.py
+++ python/term4/pr2/ex2.py
@@ -1,17 +1,31 @@
-import Tkinter
 import random
-canvas_size = 500
+import math
+import Tkinter
+canvas_size = 1000
 
-x = 250
-y = 250
-delay = 10
+x = 1
+y = 1
+delay = 1
+n = 0
+
+def sign(x):
+	if x >= 0:
+		return 1
+	else: 
+		return -1
 
 def step():
-  global x, y
-  x += random.randint(-1, 1)
-  y += random.randint(-1, 1)
-  canvas.create_line([x, y], [x + 1, y], fill='white')
-  root.after(delay, step)
+  global x, y, n
+  a, b, c = 0.4, 1, 0
+  mg = 300
+  of = 400
+  canvas.create_line([of + x * mg, of + y * mg], [of + x * mg, of + y * mg], fill='white')
+  xn = x
+  x = (y - sign(x)*math.sqrt(abs(b * x - c))) 
+  y = (a - xn)
+  n += 1
+  if n < 40000:
+  	root.after("idle", step)
 
 root = Tkinter.Tk()
 canvas = Tkinter.Canvas(root, width=canvas_size, height=canvas_size, background='black')
