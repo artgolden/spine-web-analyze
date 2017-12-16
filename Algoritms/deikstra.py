639ac1b534c5072b2a00e2371a534ada6d27ca7a
temason <temasonos@yandex.ru>
2017-12-16 20:02:22 +0300

    Wide search in Graph working withought weights

--- Algoritms/deikstra.py
+++ Algoritms/deikstra.py
@@ -1,11 +1,12 @@
 from weight_queue import QueueWithWeight
 
 class Node:
-    def __init__(self, name, weight=None, color=None):
+    def __init__(self, name, weight=None):
         self.name = name
         self.weight = weight
-        self.color = color
+        self.color = "white"
         self.children = {}
+        self.paths = []
 
 
 class Graph:
@@ -52,26 +53,53 @@ class Graph:
         return nodes_dict
 
     def print_graph(self):
+        # print self.nodes
         for i in self.nodes.values():
             print i.name
             tmp = ""
             # if len(i.paths):
                 # print i.paths
-            for j in i.children.keys():
-                if j:
-                    # j = j[0]
-                    # tmp += j.name
-                    print "children----" + j.name + str(i.children[j])
+            if i.children:
+                for j in i.children.keys():
+                    if j:
+                        # j = j[0]
+                        # tmp += j.name
+                        print "children----" + j.name + str(i.children[j])
             tmp = ""
         print "==========="
         
-        def relaxation(self, pre, nex):
-            # self.que.add()
-            old_weight = nex.weight
-            if nex.weight > (pre.weight + pre.children[nex]):
-                nex.weight = pre.weight + pre.children[nex]
-                self.que.change(nex, old_weight)
-
+    def relaxation(self, pre, nex):
+        # self.que.add()
+        old_weight = nex.weight
+        if nex.weight > (pre.weight + pre.children[nex]):
+            nex.weight = pre.weight + pre.children[nex]
+            self.que.change(nex, old_weight)
+    
+    def wide_search(self, start, end):
+        if start not in self.nodes.keys() and end not in self.nodes.keys():
+            print "start or end does not exist in self.nodes"
+            quit(1)
+        start = self.nodes[start]
+        end = self.nodes[end]
+        start.paths = [start.name]
+        start.color = "grey"
+        start.weight = 0
+        queue = [start]
+        found = False
+        while not found:
+            new_queue = []
+            for i in queue:
+                for j in i.children.keys():
+                    if j.color == "white":
+                        if j == end:
+                            # print i.paths, "hhhhhhhhhhh"
+                            print "Path: ", i.paths[0] + j.name
+                            found = True
+                        for k in i.paths:
+                            j.paths.append(k + j.name)
+                        new_queue.append(j)
+                        j.color = "grey"
+            queue = new_queue  
 
 node = Node("a", 1)
 node2 = Node("b", 2)
@@ -80,3 +108,4 @@ node2 = Node("b", 2)
 # print que.queue_dict
 graph = Graph("graph_dei.txt")
 graph.print_graph()
+graph.wide_search("a","i")
