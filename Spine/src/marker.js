94a22953d228cea768fb07914308f2a24183b285
temason <temasonos@gmail.com>
2017-12-16 23:44:50 +0300

    Add files via upload

---
+++ Spine/src/marker.js
@@ -0,0 +1,28 @@
+/**
+* Marker
+*/
+
+class Marker extends AbsoluteMarker {
+	makeMark() {
+		let mark = new Mark(this.element);
+		$(mark.element).draggable();
+		return mark;
+	}
+	markPoints(points) {
+		var point_data = [];
+		for(let i=0; i < points.length; i++) {
+			let point = points[i];
+			console.log("Point #" + point.id + ": { x: "+ point.x+", y: "+ point.y + ", place: "+ point.place );
+			this.mark(point.x, point.y,  point.id, point.place);
+			
+
+			var poi = {
+				x: point.x,
+				y: point.y,
+				id: point.id
+			};
+			point_data.push(poi);
+		}
+		// console.log(JSON.stringify(point_data));
+	}
+}
---
+++ Spine/src/spine.js
@@ -0,0 +1,47 @@
+/**
+* Editor
+*/
+
+class Editor {
+	open(url) {
+		this.loadImage(url);
+	}
+	loadImage(url) {
+		this.$image().load( function() {
+			console.log("Image loaded.");
+			Editor.getEditor().imageLoaded();
+		});
+		this.$image().attr("src", url );
+	}
+	imageLoaded() {
+		console.log("Editor::imageLoaded()");
+		let points = makePointsFromTemplate();
+		let marker = new Marker()
+		document.body.appendChild(marker.element);
+		marker.markPoints(points);
+		this.scrollToStart();
+	}
+	scrollToStart() {
+		let imageWidth = this.$image().width();
+		let imageHeight = this.$image().height();
+		if( $(window).width() < imageWidth ) {
+			this.$image().scrollLeft((imageWidth-$(window).width())/2);
+		}
+		if( $(window).height() < imageHeight ) {
+			this.$image().scrollTop(0.06*$(window).height());
+		}
+	}
+	$image() {
+		return $("img#theimage");
+	}
+
+	static getEditor() {
+		return Editor_theEditor; 
+	}
+}
+
+var Editor_theEditor = new Editor();
+
+
+const imageURL = "IMG-0001-00001.jpg";
+Editor_theEditor.open(imageURL);
